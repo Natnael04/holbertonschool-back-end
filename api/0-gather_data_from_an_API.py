18721b07a1a2db75c6c1e50cc66b09913437cce8
Natnael <>
2023-08-24 00:41:18 +0200

    fix

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -4,35 +4,33 @@ import requests
 import sys
 
 
-def get_users_information(employee_id):
-    api_url = 'https://jsonplaceholder.typicode.com/users/{employee_Id}'
+if len(sys.argv) > 1:
+    user_id = sys.argv[1]
 
-    response = requests.get(api_url)
+    url_name = f"https://jsonplaceholder.typicode.com/users/{user_id}"
+    url_todos = f"https://jsonplaceholder.typicode.com/todos?userId={user_id}"
+    name_request = requests.get(url_name)
+    todos_request = requests.get(url_todos)
 
-    if response.status_code == 200:
-        data = response.json()
-        return data
-    else:
-        print(f"Request failed with status code: {response.status_code}")
-        return None
+    if name_request.status_code == 200:
+        name_response = name_request.json()
+        name = name_response["name"]
 
-
-def main():
-    employee_id = 1
-    todo_data = get_users_information(employee_id)
-
-    if todo_data is not None:
-        EMPLOYEE_NAME = todo_data.get('EMPLOYEE_NAME')
-        NUMBER_OF_DONE_TASKS = todo_data.get('NUMBER_OF_DONE_TASKS')
-        TOTAL_NUMBER_OF_TASKS = todo_data.get('TOTAL_NUMBER_OF_TASKS')
-
-        print(
-            f"Employee {EMPLOYEE_NAME} is done with tasks ({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):")
-
-        for task in todo_data:
+    if todos_request.status_code == 200:
+        todos_response = todos_request.json()
+        completed = []
+        uncompleted = []
+        for task in todos_response:
             if task['completed']:
-                print(f"\t{'TASK_TITLE'}")
+                completed.append(task)
+            else:
+                uncompleted.append(task)
+        todos_total = (len(completed) + len(uncompleted))
 
-
-if __name__ == "__main__":
-    main()
+    output = "Employee {} is done with tasks({}/{}):".format(
+        name, len(completed), todos_total)
+    print(output)
+    for task_name in completed:
+        print(f"\t {task_name['title']}")
+else:
+    print("Please provide a user_id as a command-line argument.")
