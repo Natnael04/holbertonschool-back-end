383e2536c299d746f2709f790da8422ffe113de2
Natnael <>
2023-08-23 21:31:49 +0200

    fix the line

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,5 +1,8 @@
+#!/usr/bin/python3
+"""program that copeis all tasks in a json file"""
 import requests
 import sys
+
 
 def get_users_information(employee_id):
     api_url = 'https://jsonplaceholder.typicode.com/users/{employee_Id}'
@@ -13,20 +16,23 @@ def get_users_information(employee_id):
         print(f"Request failed with status code: {response.status_code}")
         return None
 
+
 def main():
     employee_id = 1
     todo_data = get_users_information(employee_id)
-    
+
     if todo_data is not None:
         EMPLOYEE_NAME = todo_data.get('EMPLOYEE_NAME')
         NUMBER_OF_DONE_TASKS = todo_data.get('NUMBER_OF_DONE_TASKS')
         TOTAL_NUMBER_OF_TASKS = todo_data.get('TOTAL_NUMBER_OF_TASKS')
 
-        print(f"Employee {EMPLOYEE_NAME} is done with tasks ({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):")
-        
+        print(
+            f"Employee {EMPLOYEE_NAME} is done with tasks ({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):")
+
         for task in todo_data:
             if task['completed']:
                 print(f"\t{'TASK_TITLE'}")
+
 
 if __name__ == "__main__":
     main()
