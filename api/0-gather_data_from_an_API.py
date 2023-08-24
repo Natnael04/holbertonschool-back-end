3907fbef91c435a62d1ce3042aff1a8c91252121
Natnael <>
2023-08-24 10:14:39 +0200

    fix the comments

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -5,22 +5,23 @@ import sys
 
 
 def get_employee_todo_progress(employee_id):
+    """get employee information based on id"""
     base_url = "https://jsonplaceholder.typicode.com"
 
-    # Get employee details
+
     user_response = requests.get(f"{base_url}/users/{employee_id}")
     user_data = user_response.json()
     employee_name = user_data["name"]
 
-    # Get employee's TODO list
+
     todo_response = requests.get(f"{base_url}/users/{employee_id}/todos")
     todo_data = todo_response.json()
 
-    # Count completed and total tasks
+
     total_tasks = len(todo_data)
     completed_tasks = sum(1 for task in todo_data if task["completed"])
 
-    # Print progress information
+
     print(
         f"Employee {employee_name} is done with tasks ({completed_tasks}/{total_tasks}):")
 
