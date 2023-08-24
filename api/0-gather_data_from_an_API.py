c4e134535a1fc304c18acfbee7e710c49254e085
Natnael <>
2023-08-24 10:29:26 +0200

    fix the space

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -8,23 +8,17 @@ def get_employee_todo_progress(employee_id):
     """get employee information based on id"""
     base_url = "https://jsonplaceholder.typicode.com"
 
-
     user_response = requests.get(f"{base_url}/users/{employee_id}")
     user_data = user_response.json()
     employee_name = user_data["name"]
 
-
     todo_response = requests.get(f"{base_url}/users/{employee_id}/todos")
     todo_data = todo_response.json()
 
-
     total_tasks = len(todo_data)
     completed_tasks = sum(1 for task in todo_data if task["completed"])
-
-
     print(
         f"Employee {employee_name} is done with tasks({completed_tasks}/{total_tasks}):")
-
     for task in todo_data:
         if task["completed"]:
             print(f"\t {task['title']}")
