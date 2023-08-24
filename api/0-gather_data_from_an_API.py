8190f58a25ae39f39336a8ef5a75e3e91cbc423d
Natnael <>
2023-08-24 10:18:40 +0200

    delete the space

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -23,7 +23,7 @@ def get_employee_todo_progress(employee_id):
 
 
     print(
-        f"Employee {employee_name} is done with tasks ({completed_tasks}/{total_tasks}):")
+        f"Employee {employee_name} is done with tasks({completed_tasks}/{total_tasks}):")
 
     for task in todo_data:
         if task["completed"]:
