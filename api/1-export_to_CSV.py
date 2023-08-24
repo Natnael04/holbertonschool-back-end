fe4b200a60239633d38e8d1049431d39124bde31
Natnael <>
2023-08-24 13:47:32 +0200

    export csv

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,20 +1,46 @@
-#!/usr/bin/python3
 import csv
+import requests
+import sys
 
-# Sample data
-data = [
-    ["USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"]
-]
 
-# Specify the CSV file path
-csv_file_path = "USER_ID.csv"
+def export_employee_tasks_to_csv(employee_id, employee_name, tasks_data):
+    csv_file_path = f"{employee_id}.csv"
 
-# Open the CSV file for writing
-with open(csv_file_path, mode="w", newline="") as file:
-    writer = csv.writer(file)
-    
-    # Write the data to the CSV file row by row
-    for row in data:
-        writer.writerow(row)
+    with open(csv_file_path, mode="w", newline="") as file:
+        writer = csv.writer(file)
 
-print("Data exported to CSV successfully.")
+        # Write headers
+        headers = ["USER_ID", "USERNAME",
+                   "TASK_COMPLETED_STATUS", "TASK_TITLE"]
+        writer.writerow(headers)
+
+        # Write tasks data
+        for task in tasks_data:
+            task_completed = "True" if task["completed"] else "False"
+            writer.writerow([employee_id, employee_name,
+                            task_completed, task["title"]])
+
+    print(
+        f"Tasks for employee {employee_name} exported to {csv_file_path} successfully.")
+
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <employee_id>")
+        sys.exit(1)
+
+    employee_id = int(sys.argv[1])
+    base_url = "https://jsonplaceholder.typicode.com"
+
+    user_response = requests.get(f"{base_url}/users/{employee_id}")
+    user_data = user_response.json()
+    employee_name = user_data["username"]
+
+    tasks_response = requests.get(f"{base_url}/todos?userId={employee_id}")
+    tasks_data = tasks_response.json()
+
+    export_employee_tasks_to_csv(employee_id, employee_name, tasks_data)
+
+
+if __name__ == "__main__":
+    main()
